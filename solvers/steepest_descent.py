6c54509dd1e052ece6b533501bae182c6f57f7c6
Samyak Datta <datta.samyak@gmail.com>
2019-04-11 20:59:13 -0400

    Minor re-org for steepest descent

--- solvers/steepest_descent.py
+++ solvers/steepest_descent.py
@@ -5,12 +5,11 @@ class SteepestDescentSolver():
     def __init__(self, fn, x0, alpha, iter=0, term_crit='fn'):
         self.EPS = 1e-6
         self.fn = fn
-        self.curr_iterate = x0
         self.alpha = alpha
         self.iter = iter
         self.term_criteria = term_crit
 
-        self.update(self.curr_iterate)
+        self.update(x0)
 
         '''
             Caching because required for evaluating termination criteria
@@ -19,16 +18,15 @@ class SteepestDescentSolver():
         self.grad_f_x0_norm = copy.copy(self.grad_fx_norm)
 
     def update(self, x):
-        self.fx = self.fn.evaluate(self.curr_iterate)
-        self.grad_fx = self.fn.gradient(self.curr_iterate)
+        self.curr_iterate = x
+        self.fx = self.fn.evaluate(x)
+        self.grad_fx = self.fn.gradient(x)
         self.grad_fx_norm = np.linalg.norm(self.grad_fx)
 
     def step(self):
         self.iter += 1
         next_iterate = self.curr_iterate - self.alpha * self.grad_fx
-
-        self.curr_iterate = next_iterate
-        self.update(self.curr_iterate)
+        self.update(next_iterate)
 
     def termination_criteria_fn(self):
         if (self.fx / self.f_x0) <= self.EPS: return True
