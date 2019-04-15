627681339fb825dbec82fe6f3c985dd7f3d03e37
Nirbhay Modhe <nirbhaym@gatech.edu>
2019-04-15 17:02:50 -0400

    Refactored solvers and added a base class

---
+++ solvers/base_solver.py
@@ -0,0 +1,62 @@
+import copy
+import numpy as np
+
+import copy
+import numpy as np
+
+from line_search import LineSearchMethod
+
+class BaseSolver(object):
+    def __init__(
+        self,
+        fn,
+        x0,
+        alpha=None,
+        iter=0,
+        term_crit='fn',
+        use_line_search=False,
+        ls_method_kwargs=None,
+    ):
+        self.EPS = 1e-6
+        self.fn = fn
+        self.alpha = alpha
+        self.iter = iter
+        self.term_criteria = term_crit
+        self.use_line_search = use_line_search
+
+        if use_line_search:
+            self.ls_method = LineSearchMethod(**ls_method_kwargs)
+        else:
+            assert self.alpha is not None
+            self.ls_method = None
+
+        self.update(x0)
+
+        '''
+            Caching because required for evaluating termination criteria
+        '''
+        self.f_x0 = copy.copy(self.fx)
+        self.grad_f_x0_norm = copy.copy(self.grad_fx_norm)
+
+    def update(self, x):
+        self.curr_iterate = x
+        self.fx = self.fn.evaluate(x)
+        self.grad_fx = self.fn.gradient(x)
+        self.grad_fx_norm = np.linalg.norm(self.grad_fx)
+
+    def step(self):
+        raise NotImplementedError("Child class must implement step function!")
+
+    def termination_criteria_fn(self):
+        if (self.fx / self.f_x0) <= self.EPS: return True
+        return False
+
+    def termination_criteria_grad(self):
+        if self.grad_fx_norm <= self.EPS * (self.grad_f_x0_norm + 1.): return True
+        return False
+
+    def termination_criteria_reached(self):
+        if self.term_criteria == 'fn':
+            return self.termination_criteria_fn()
+        else:
+            return self.termination_criteria_grad()
--- solvers/conjugate_gradient.py
+++ solvers/conjugate_gradient.py
@@ -1,9 +1,11 @@
 import copy
 import numpy as np
 
+from .base_solver import BaseSolver
+
 from line_search import LineSearchMethod
 
-class ConjugateGradientSolver():
+class ConjugateGradientSolver(BaseSolver):
     def __init__(
         self,
         fn,
@@ -15,35 +17,18 @@ class ConjugateGradientSolver():
         use_line_search=False,
         ls_method_kwargs=None,
     ):
-        self.EPS = 1e-6
-        self.fn = fn
-        self.alpha = alpha
-        self.iter = iter
-        self.term_criteria = term_crit
         self.variant = variant
         self.n = x0.shape[0]
-        self.use_line_search = use_line_search
-
-        if use_line_search:
-            self.ls_method = LineSearchMethod(**ls_method_kwargs)
-        else:
-            assert self.alpha is not None
-            self.ls_method = None
-
-        self.update(x0)
+        super().__init__(
+            fn=fn,
+            x0=x0,
+            alpha=alpha,
+            iter=iter,
+            term_crit=term_crit,
+            use_line_search=use_line_search,
+            ls_method_kwargs=ls_method_kwargs,
+        )
         self.curr_direction = -1. * self.grad_fx
-
-        '''
-            Caching because required for evaluating termination criteria
-        '''
-        self.f_x0 = copy.copy(self.fx)
-        self.grad_f_x0_norm = copy.copy(self.grad_fx_norm)
-
-    def update(self, x):
-        self.curr_iterate = x
-        self.fx = self.fn.evaluate(self.curr_iterate)
-        self.grad_fx = self.fn.gradient(self.curr_iterate)
-        self.grad_fx_norm = np.linalg.norm(self.grad_fx)
 
     def compute_beta(self, prev_grad, curr_grad):
         curr_grad_norm = np.linalg.norm(curr_grad)
@@ -88,18 +73,3 @@ class ConjugateGradientSolver():
         # Compute next direction: d_{k+1} and update self.curr_direction
         next_direction = -1. * self.grad_fx + beta * self.curr_direction
         self.curr_direction = next_direction
-
-
-    def termination_criteria_fn(self):
-        if (self.fx / self.f_x0) <= self.EPS: return True
-        return False
-
-    def termination_criteria_grad(self):
-        if self.grad_fx_norm <= self.EPS * (self.grad_f_x0_norm + 1.): return True
-        return False
-
-    def termination_criteria_reached(self):
-        if self.term_criteria == 'fn':
-            return self.termination_criteria_fn()
-        else:
-            return self.termination_criteria_grad()
